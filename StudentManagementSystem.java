19925d792814f8292d068256ce57c33121232520
Rapidgh <dakyeremeh@palm.edu.gh>
2025-03-14 00:27:56 +0000

    Update StudentManagementSystem.java

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -3,29 +3,29 @@ import java.util.*;
 
 public class StudentManagement {
     public static void main(String[] args) {
-        
-        String studentID = "101";
+
+        String studentID = "001";
         String studentName = "Derrick Agyei Kyeremeh";
-        int studentAge = 20;
+        int studentAge = 21;
         List<String> coursesEnrolled = new ArrayList<>();
         double GPA;
 
-        String courseID = "CSE101";
-        String courseName = "Computer Science";
+    
+        String courseName = "(DBIT101) - Principles of Programming(Java)  , (PLC107) - African Studies";
         int courseCredits = 3;
-
-       
+        
         coursesEnrolled.add(courseName);
-        System.out.println(studentName + " enrolled in " + courseName);
+        System.out.println(studentName + "  " + courseName);
 
-       
         Random rand = new Random();
-        GPA = 2.0 + (rand.nextDouble() * 2.0); 
+        GPA = 3.0 + (rand.nextDouble() * 2); 
         System.out.println("GPA calculated: " + GPA);
-
-       
-        System.out.println("Student ID: " + studentID + ", Name: " + studentName + ", Age: " + studentAge + ", GPA: " + GPA);
-        System.out.println("Courses Enrolled: " + coursesEnrolled);
+        
+        System.out.println("Student ID: " + studentID);  
+        System.out.println("Name: " + studentName);  
+        System.out.println("Age: " + studentAge);  
+        System.out.println("GPA: " + GPA);  
+        System.out.println("Courses Enrolled: " + courseName); 
 
        
     }
